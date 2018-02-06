127da81346113532f918b56865f181f5986b3a36
Hitoshi Kamezaki <kame@info-cloud.biz>
2018-02-06 14:53:13 +0900

    add stub of token api

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 import apiRoutes from './routes/api.route';
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use('/api', apiRoutes);
 
--- src/routes/api.route.ts
+++ src/routes/api.route.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
 
 const api: Router = Router();
 
 api.use(cors());
 api.get('/', function(req, res, next) {
     res.send('something message');
+});
+
+api.post('/token', function(req, res, next) {
+    console.log('params key [' + req.body.key + '] sec [' + req.body.sec + ']' );
+    res.send('something to send');
 });
 
 export default api;
